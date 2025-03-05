c5a64748f5250333c6329c142897806e0be718f0
Tio793 <tiotio3175@gmail.com>
2025-03-06 00:32:45 +0700

    css masking

--- script.js
+++ script.js
@@ -12,4 +12,55 @@ document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbar.contains(e.target)) {
     navbar.classList.remove("active");
   }
+
+  if (!search.contains(e.target) && !searchIcon.contains(e.target)) {
+    search.classList.remove("active");
+  }
+
+  if (!cart.contains(e.target) && !shopCart.contains(e.target)) {
+    cart.classList.remove("active");
+  }
 });
+
+const searchIcon = document.getElementById("search");
+const search = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.getElementById("search").onclick = (e) => {
+  search.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+const shopCart = document.getElementById("shoping-cart-button");
+const cart = document.querySelector(".shooping-cart");
+
+document.getElementById("shoping-cart-button").onclick = (e) => {
+  cart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// modal box
+
+const itemDetailModal = document.getElementById("item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
+
+itemDetailButton.forEach((btn) => {
+  btn.onclick = (e) => {
+    e.preventDefault();
+    itemDetailModal.style.display = "flex";
+  };
+});
+
+// click tombol close
+
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
